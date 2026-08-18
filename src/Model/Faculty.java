dd7ea20b52a9c369ba61377c3fdb4ac29bc76bc4
chanhisme <chanhle1907@gmail.com>
2026-08-18 10:32:44 +0700

    add credits attribute

--- src/Model/Faculty.java
+++ src/Model/Faculty.java
@@ -1,4 +1,27 @@
 package Model;
 
 public class Faculty {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Faculty(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
--- src/Model/Subject.java
+++ src/Model/Subject.java
@@ -3,12 +3,13 @@ package Model;
 public class Subject {
     private String id;
     private String name;
+    private int credits;
 
-    public Subject(String id, String name) {
+    public Subject(String id, String name, int credits) {
         this.id = id;
         this.name = name;
+        this.credits = credits;
     }
-
     public String getId() {
         return id;
     }
@@ -24,4 +25,14 @@ public class Subject {
     public void setName(String name) {
         this.name = name;
     }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+
 }
